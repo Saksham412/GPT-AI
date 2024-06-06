10dab69e0dc0fe920f2a3241b042dae1692c0086
Saksham <sakshamshinde412@gmail.com>
2024-06-06 23:37:06 +0530

    updated authcontroller

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const { registerController, loginController, logoutController } = require('../controllers/authController')
+const { registerContoller, loginController, logoutController } = require('../controllers/authController')
 
 //router object
 const router = express.Router()
 
 //routes
 //register
-router.post('/register', registerController)
+router.post('/register', registerContoller)
 
 //login
 router.post('/login', loginController)
--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const bodyParser = require("body-parser");
 const colors = require("colors");
 const dotenv = require("dotenv");
 const connectDB = require('./config/db');
+const errorHandler = require('./middlewares/errorMiddleware');
+
 //routes path
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
 
 
 //dotenv
@@ -28,7 +30,7 @@ const PORT = process.env.PORT || 8080;
 
 //API routes
 app.use('/api/v1/auth', authRoutes)
-
+app.use(errorHandler)
 //listen server
 app.listen(PORT, () => {
     console.log(
