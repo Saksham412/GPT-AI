4a9fc3eaf2d892929940523930f4aac4d9473852
Saksham <sakshamshinde412@gmail.com>
2024-06-07 11:05:52 +0530

    Buildin components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import { useMemo } from 'react';
+import { CssBaseline, ThemeProvider} from "@mui/material";
+import {createTheme} from "@mui/material/styles"
+import {themeSettings} from "./theme"
+import Navbar from './components/Navbar';
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings(), []))
+
   return (
-    <div>
-      <h1>Welcome</h1>
-    </div>
+    <>
+    <ThemeProvider theme={theme}>
+    <CssBaseline/>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+    </ThemeProvider>
+    </>
   );
 }
 
